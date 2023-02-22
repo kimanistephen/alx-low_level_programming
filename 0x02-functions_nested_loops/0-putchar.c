425d9a61f6edbaf183516310c61086715776fd6d
stephen kimani <kimanistephen754@gmail.com>
2023-02-22 06:39:30 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print -putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+init main(void)
+{
+_putchar('_');
+-putchar('p');
+-putchar('u');
+-putchar('t');
+-putchar('c');
+-putchar('h');
+-putchar('a');
+-putchar('r');
+-putchar('\n');
+return (0);
+}
