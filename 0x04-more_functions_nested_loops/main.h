6c37529c176405fee6230aeea3cdce3b475099d0
stephen kimani <kimanistephen754@gmail.com>
2023-02-23 21:43:49 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-init _putchar(char c);
+#ifindef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,5 +10,8 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
- void print_triangle(int size);
- void print_number(int n);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif /* MAIN_H */
+
